f2dd14d6efbbb1f3df913819f4714cc62f7835c2
0eti <samuelzhang012@gmail.com>
2023-05-25 22:13:30 -0700

    going to try to incorporate ts and webpack to bundle to js files to run in console

---
+++ src/js/test.js
@@ -0,0 +1,6 @@
+console.log("FUCKING FUCK");
+
+const freezeBlockElem = document.getElementById("freezeBlock");
+console.log(freezeBlockElem);
+
+const clickListener = () => {};
--- test.js
+++
@@ -1,4 +0,0 @@
-console.log("FUCKING FUCK");
-
-freezeBlockElem = document.getElementById("freezeBlock");
-console.log(freezeBlockElem);
